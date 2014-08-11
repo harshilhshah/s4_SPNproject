8169e2aa2f9fddc729880f30fd9d5557c4b9a51d
Dylan Sellar <sellar.dylan@gmail.com>
2014-08-10 20:21:15 -0400

    Changed ProfLoginServlet Reference

--- src/spn_test/ProfLoginServlet.java
+++ src/spn_test/ProfLoginServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet("/ProfLoginServlet")
 public class ProfLoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
